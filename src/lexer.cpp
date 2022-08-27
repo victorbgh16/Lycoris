186d4ae591b69fabb0aca4ad4b179ede6bbc2fb0
victor.bgh <victor.berghausen@gmx.de>
2022-08-27 22:52:34 +0200

    finished basic lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,7 +1,4 @@
-//
 // Created by Victor Berghausen on 27.08.2022.
-//
-
 #include <string>
 
 enum Token {
@@ -18,6 +15,57 @@ enum Token {
 
 static std::string IdentifierStr;
 static double NumVal;
+
+// get_token: return the next token from standard input
+static int get_token() {
+    static int LastChar = ' ';
+
+    // Skip any whitespace
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return tok_def;
+        if (IdentifierStr == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+        // Comment until end of line.
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+
+    // Check for end of file.  Don't eat the EOF.
+    if (LastChar == EOF)
+        return tok_eof;
+
+    // Otherwise, just return the character as its ASCII value.
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
 
 int main() {
 
