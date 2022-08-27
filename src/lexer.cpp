269382791ff33aff71c23e80ad14b86ed6ddab3b
victor.bgh <victor.berghausen@gmx.de>
2022-08-27 22:16:46 +0200

    changed lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,3 +1,25 @@
 //
-// Created by victo on 27.08.2022.
+// Created by Victor Berghausen on 27.08.2022.
 //
+
+#include <string>
+
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
+};
+
+static std::string IdentifierStr;
+static double NumVal;
+
+int main() {
+
+    return 0;
+}
