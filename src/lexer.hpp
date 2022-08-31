4e9fae1cc1518d8dd3a396cccdacfae975d14b9e
victor.bgh <victor.berghausen@gmx.de>
2022-08-31 11:25:24 +0200

    edit lexer.hpp

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -13,26 +13,138 @@ namespace lyc {
         enum TokType {
             INT,
             FLT,
+
             CHAR,
             STR,
             IDEN,
 
+            // Keywords
+            LET,
+            DEF,
             IF,
             ELIF,
             ELSE,
-            RETURN,
-            CONTINUE,
-            BREAK,
             FOR,
             IN,
             WHILE,
+            RETURN,
+            CONTINUE,
+            BREAK,
+            VOID,
+            TRUE,
+            FALSE,
+            NIL,
+            ANY,  // type: any
+            TYPE, // type: type
+            I1,
+            I8,
+            I16,
+            I32,
+            I64,
+            U8,
+            U16,
+            U32,
+            U64,
+            F32,
+            F64,
+            OR,
+            STATIC,
+            CONST,
+            VOLATILE,
+            DEFER,
+            EXTERN,
+            COMPTIME, // maybe?
+            GLOBAL,
+            INLINE, // maybe?
+            STRUCT,
+            ENUM,
 
-            IMPORT,
-            AS,
-            FROM,
+            // Operators
+            ASSN,
+            // Arithmetic
+            ADD,
+            SUB,
+            MUL,
+            DIV,
+            MOD,
+            ADD_ASSN,
+            SUB_ASSN,
+            MUL_ASSN,
+            DIV_ASSN,
+            MOD_ASSN,
+            // Post/Pre Inc/Dec
+            XINC,
+            INCX,
+            XDEC,
+            DECX,
+            // Unary
+            UADD,
+            USUB,
+            UAND, // address of
+            UMUL, // dereference
+            // Logic
+            LAND,
+            LOR,
+            LNOT,
+            // Comparison
+            EQ,
+            LT,
+            GT,
+            LE,
+            GE,
+            NE,
+            // Bitwise
+            BAND,
+            BOR,
+            BNOT,
+            BXOR,
+            BAND_ASSN,
+            BOR_ASSN,
+            BNOT_ASSN,
+            BXOR_ASSN,
+            // Others
+            LSHIFT,
+            RSHIFT,
+            LSHIFT_ASSN,
+            RSHIFT_ASSN,
 
-            DEF,
+            SUBS, // idk what this is
+
+            FNCALL, // function call and struct template specialization
+            STCALL, // instantiate structs
+
+            // Varargs
+            PreVA,
+            PostVA,
+
+            // Separators
+            DOT,
+            QUEST,
+            COL,
+            COMMA,
+            AT,
+            SPC,
+            TAB,
+            NEWL,
+            COLS, // Semi colon
+            ARROW, // maybe?
+            // Parenthesis, Braces, Brackets
+            LPAREN,
+            RPAREN,
+            LBRACE,
+            RBRACE,
+            LBRACK,
+            RBRACK,
+
+            FEOF,
+            INVALID,
+
+            _LAST,
         };
+
+        extern const char *TokStrs[_LAST];
+
+
     } // namespace lex
 } // namespace lyc
 
